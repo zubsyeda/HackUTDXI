8f1520fe950163e07e9df65999dc99a4a5634aeb
zubsyeda <125157533+zubsyeda@users.noreply.github.com>
2024-11-16 19:30:44 -0600

    FIXED TAILWIND ERRORRS

--- front-end/my-next-app/tailwind.config.ts
+++ front-end/my-next-app/tailwind.config.ts
@@ -1,5 +1,14 @@
-import type { Config } from "tailwindcss";
+/** @type {import('tailwindcss').Config} */
+
 module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+ 
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   theme: {
     extend: {
       fontFamily: {
@@ -7,21 +16,5 @@ module.exports = {
       },
     },
   },
-  // ... rest of your config
-}
-export default {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
   plugins: [],
-} satisfies Config;
+}
